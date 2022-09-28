ceca716686b96a53dcabb9a8529a0094b58f9942
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2022-09-28 08:23:30 -0300

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,9 @@
+<template lang="html">
+<div>
+    <p ref="palavra">texto</p>
+</div>
+</template>
+
+   console.log("this.$refs", this.$refs.palavra)
+   
+  console.log("this.$refs", this.$refs.palavra.clientWidth)
